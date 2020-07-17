78395d18935f55356616e25b46655291add7cb8c
Pedro Sousa <ppls2106@gmail.com>
2020-07-17 11:02:21 -0300

    add getData function to return json

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main(List<String> args) {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const _request_key = '07194033';
+const _request_base = 'https://api.hgbrasil.com/finance';
+const _request_api = '$_request_base/?format=json&key=$_request_key';
+
+void main(List<String> args) async {
   runApp(
     MaterialApp(
       title: 'Conversor de Moedas',
       home: Home(),
     ),
   );
+}
+
+Future<Map> getData() async {
+  var response = await http.get(_request_api);
+  return json.decode(response.body);
 }
 
 class Home extends StatefulWidget {
