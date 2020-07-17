2f388e206829718876e06d35baba46230d721105
Pedro Sousa <ppls2106@gmail.com>
2020-07-17 12:29:07 -0300

    apply decimal keyboard on IOS and clear all if any is empty

--- lib/main.dart
+++ lib/main.dart
@@ -45,21 +45,39 @@ class _HomeState extends State<Home> {
   double euro;
 
   void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
     double real = double.parse(text);
     dolarController.text = (real / dolar).toStringAsFixed(2);
     euroController.text = (real / euro).toStringAsFixed(2);
   }
 
   void _dolarChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
     double dolar = double.parse(text);
     realController.text = (dolar * this.dolar).toStringAsFixed(2);
     euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
   }
 
   void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
     double euro = double.parse(text);
     dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
     realController.text = (euro * this.euro).toStringAsFixed(2);
+  }
+
+  void _clearAll() {
+    realController.text = '';
+    dolarController.text = '';
+    euroController.text = '';
   }
 
   @override
@@ -133,7 +151,7 @@ Widget buildTextField(
     String label, String prefix, TextEditingController controller, Function c) {
   return TextField(
     controller: controller,
-    keyboardType: TextInputType.number,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
     decoration: InputDecoration(
       labelText: label,
       labelStyle: TextStyle(
